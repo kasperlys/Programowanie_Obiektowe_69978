f87adadb1e1ce57bd39ae0cdb064866ff4bbf660
kasperlys <antikshowsk@gmail.com>
2025-12-08 13:28:57 +0100

    lab_4 ready

---
+++ lab_4/lab_4/lab_4-6.cs
@@ -0,0 +1,9 @@
+﻿Queue<int> queue = new Queue<int>();
+queue.Enqueue(10);
+queue.Enqueue(20);
+Console.WriteLine(queue.Dequeue()); // 10
+
+Stack<int> stack = new Stack<int>();
+stack.Push(10);
+stack.Push(20);
+Console.WriteLine(stack.Pop()); // 20
